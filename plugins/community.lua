8d694eab420e8ca47c354a473164ef0dd412688a
machearn <machearn.n@gmail.com>
2023-12-09 17:52:41 -0500

    stable version 1 config

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,4 +8,19 @@ return {
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.colorscheme.sonokai" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = { "80", "100", "120" },
+      custom_colorcolumn = {
+        gitcommit = "72",
+      },
+    },
+  },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -15,9 +15,9 @@ return {
     opts = {
       keywords = {
         TODO = { icon = " ", color = "info" },
-        WARN = { icon = " ", color = "warning", alt = { } },
+        WARN = { icon = " ", color = "warning", alt = {} },
         PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = " ", color = "hint", alt = { } },
+        NOTE = { icon = " ", color = "hint", alt = {} },
         TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
       },
     },
@@ -27,20 +27,25 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    ---@type Flash.Config
     opts = {},
-    -- stylua: ignore
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      {
+        "sf",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump { search = { forward = true, wrap = false, multi_window = false } } end,
+        desc = "Flash Forward",
+      },
+      {
+        "sF",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump { search = { forward = false, wrap = false, multi_window = false } } end,
+        desc = "Flash Backward",
+      },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
-  },
-  {
-    "github/copilot.vim",
-    event = { "User AstroFile" },
   },
   {
     "onsails/lspkind.nvim",
@@ -61,5 +66,63 @@ return {
       }
       return opts
     end,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "User AstroFile",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        debounce = 150,
+        keymap = {
+          accept = "<C-l>",
+        },
+      },
+    },
+  },
+  {
+    "mg979/vim-visual-multi",
+    event = "VeryLazy",
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-k>",
+        ["Find Subword Under"] = "<C-k>",
+        ["Add Cursor Down"] = "<M-j>",
+        ["Add Cursor Up"] = "<M-k>",
+      }
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        hover = {
+          enabled = false,
+        },
+        signature = {
+          enabled = false,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
   },
 }
